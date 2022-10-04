ab70267ceb2ea0b9034f630e866b900d42135bc6
Jaspreet Kaur <kaurjaspreet9795@gmail.com>
2022-10-04 09:43:25 -0700

    add damage multiplier calculation method

--- index.js
+++ index.js
@@ -39,3 +39,50 @@ function bottomRightStats(statsMenu) {
 }
 
 
+function isTypeInDamageArray(pokemonType, damageArray) {
+    for (let i = 0; i < damageArray.length; i++) {
+        if(damageArray[i]['name'] == pokemonType) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function getDamageMultiplier(attackPokemonType, defendingPokemonType) {
+    var pokemonTypes = [];
+    fetch(`https://pokeapi.co/api/v2/type`)
+    .then(res => {
+        res.json();
+    })
+    .then(res => {
+        pokemonTypes = res['results'];
+    })
+    console.log(pokemonTypes);
+
+    var attackPokemonIndex = 0;
+    var defendingPokemonIndex = 0;
+    for (let i = 0; i < pokemonTypes.length; i++) {
+        if (pokemonTypes[i] == attackPokemonType) {
+            attackPokemonIndex = i;
+        }
+        if (pokemonTypes[i] == defendingPokemonType) {
+            defendingPokemonIndex = i;
+        }
+    }
+
+    fetch(`https://pokeapi.co/api/v2/type/${attackPokemonIndex+1}`).then(res => res.json())
+    .then(damageInfo => {
+        multipliers = damageInfo['damage_relations']
+        if (isTypeInDamageArray(defendingPokemonType, multipliers['double_damage_to'])) {
+            return 2;
+        }
+        if (isTypeInDamageArray(defendingPokemonType, multipliers['half_damage_to'])) {
+            return 0.5;
+        }
+        else {
+            return 0;
+        }
+    })
+}
+
+getDamageMultiplier("normal", "rock");
